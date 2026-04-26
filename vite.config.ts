84aae6b18d0567b7a1289b9f1515af4a61a92762
Hamza mac <hamza.sallam.dev@outlook.com>
2026-04-27 01:10:36 +0300

    chore: update favicon to jpeg, remove old icon, and add allowed host to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["horizon-vista.hamzasallam.online"],
     hmr: {
       overlay: false,
     },
